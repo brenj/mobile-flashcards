21124dc0b7af2abd3a06ac2eb6035dfbae5311b6
brenj <brenj@mailbox.org>
2018-01-13 20:43:13 -0600

    Add NewDeck component

---
+++ component/NewDeck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function NewDeck() {
+  return (
+    <View>
+      <Text>New deck!</Text>
+    </View>
+  );
+}
+
+export default NewDeck;
