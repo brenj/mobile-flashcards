9aa4105e1a02d0369ef887aec1fff72d584b4777
brenj <brenj@mailbox.org>
2018-01-13 20:42:03 -0600

    Add DeckList component

---
+++ component/DeckList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FlatList, View } from 'react-native';
+
+import DeckListItem from './DeckListItem';
+
+const decks = [
+  { key: 1, name: 'Test 1', cards: 5 },
+  { key: 2, name: 'Test 2', cards: 5 },
+  { key: 3, name: 'Test 3', cards: 5 },
+  { key: 4, name: 'Test 4', cards: 5 },
+  { key: 5, name: 'Test 5', cards: 5 },
+];
+
+function DeckList() {
+  return (
+    <View>
+      <FlatList
+        data={decks}
+        renderItem={({ item }) => <DeckListItem {...item} />}
+      />
+    </View>
+  );
+}
+
+export default DeckList;
