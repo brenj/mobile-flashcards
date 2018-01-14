107b44206da4419e5166207a53d0e258052ccc76
brenj <brenj@mailbox.org>
2018-01-14 17:28:45 -0600

    Add initial API for accessing asynchronous storage

---
+++ util/api.js
@@ -0,0 +1,9 @@
+export function getDecks() {
+  const decks = {
+    1: { key: 1, name: 'Test 1', cards: 5  },
+    2: { key: 2, name: 'Test 2', cards: 5  },
+    3: { key: 3, name: 'Test 3', cards: 5  },
+    4: { key: 4, name: 'Test 4', cards: 5  },
+  };
+  return decks;
+}
