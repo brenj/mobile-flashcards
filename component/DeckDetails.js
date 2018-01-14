541a38d53ecea503fefd4a0f77c89bb33c91717e
brenj <brenj@mailbox.org>
2018-01-13 20:44:13 -0600

    Add DeckDetails component

---
+++ component/DeckDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function DeckDetails() {
+  return (
+    <View>
+      <Text>Deck details</Text>
+    </View>
+  );
+}
+
+export default DeckDetails;
