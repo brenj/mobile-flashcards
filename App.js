0d0996f2491becc25db0eda9cd1f8feea6ec1435
brenj <brenj@mailbox.org>
2018-01-15 17:46:36 -0600

    Update deck details header title

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const AppNavigator = StackNavigator({
       headerStyle: {
         backgroundColor: blue,
       },
-      title: 'Decks'
+      title: 'Details'
     },
     screen: DeckDetails,
   },
