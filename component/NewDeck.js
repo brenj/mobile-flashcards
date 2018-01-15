75d99d9f4f569cdb81f6da2cc49f42ee8a9b83b7
brenj <brenj@mailbox.org>
2018-01-15 00:01:21 -0600

    Save new decks to asynchronous storage

--- component/NewDeck.js
+++ component/NewDeck.js
@@ -7,6 +7,7 @@ import {
 import { connect } from 'react-redux';
 
 import { addDeck } from '../action';
+import { saveDeckTitle } from '../util/api';
 
 class NewDeck extends React.Component {
 
@@ -18,12 +19,13 @@ class NewDeck extends React.Component {
 
   handleSubmit = () => {
     const { dispatch, navigation } = this.props;
-    const newDeck = {
-      50: {key: 50, name: this.state.title, cardCount: 0},
-    };
+    const { title } = this.state;
 
-    // save to asynchronous storage
-    dispatch(addDeck(newDeck));
+    saveDeckTitle(title).then(() => {
+      const newDeck = {
+        [title]: { key: title, title, cards: [], cardCount: 0 }};
+      dispatch(addDeck(newDeck));
+    });
 
     // Reset NewDeck UI
     this.setState({ title: '' });
