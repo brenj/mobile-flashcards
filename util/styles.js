c4b569682b8d3f4058456d4131fa16601b633c5a
brenj <brenj@mailbox.org>
2018-01-17 23:41:31 -0600

    Add global styles

---
+++ util/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  centeredContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
