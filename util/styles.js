af7c142e93f29f3da2b6e53e3d63fa05f3ab4fd0
brenj <brenj@mailbox.org>
2018-01-18 22:47:24 -0600

    Add global styles

--- util/styles.js
+++ util/styles.js
@@ -1,9 +1,40 @@
 import { StyleSheet } from 'react-native';
 
-export const globalStyles = StyleSheet.create({
+import { gray, gold } from './colors';
+
+export default StyleSheet.create({
   centeredContainer: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
+  },
+  mediumText: {
+    fontSize: 20,
+  },
+  largeText: {
+    fontSize: 30,
+  },
+  primaryButton: {
+    alignItems: 'center',
+    backgroundColor: gray,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 15,
+    width: 150,
+  },
+  secondaryButton: {
+    alignItems: 'center',
+    backgroundColor: gold,
+    borderRadius: 10,
+    padding: 10,
+    width: 150,
+  },
+  textInput: {
+    borderColor: gray,
+    borderWidth: 1,
+    height: 40,
+    marginTop: 10,
+    marginBottom: 25,
+    width: 300,
   },
 });
