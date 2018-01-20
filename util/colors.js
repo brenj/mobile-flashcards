b756c259f522878c83e8c6ed9d03e677156d4448
brenj <brenj@mailbox.org>
2018-01-19 19:14:07 -0600

    Update shade of gray used in app

--- util/colors.js
+++ util/colors.js
@@ -1,5 +1,5 @@
 export const crimson = '#b50c45';
 export const gold = '#fd9d33';
-export const gray = '#c0c0c0';
+export const gray = '#8484ad';
 export const green = '#18b78a';
 export const red = '#b71845';
